dc275c0e26ea02a49156154f8f329562b8099d8a
Brandon Furr <brandon.furr123@gmail.com>
2019-09-26 19:57:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,3 +11,11 @@ def happy_birthday(birthday_kids)
 end
 
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.collect do |name, age|
+    if age < 13 puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this."
+  end
+end
+end
